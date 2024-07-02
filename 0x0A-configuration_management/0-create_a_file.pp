90c72ccf68700e55f81bd92802e38eee1078a910
youssef <youssefahmedabdelkader25@gmail.com>
2024-07-02 08:29:52 +0300

    final

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # This Puppet Create The School File
 file { '/tmp/school':
   ensure  => 'present',
-  mode    => '044',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
+  path    => '/tmp/school',
 }
