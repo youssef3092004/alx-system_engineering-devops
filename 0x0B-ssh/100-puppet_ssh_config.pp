aa2328ed58238eaa8558b842bfd4948f4e7a3306
youssef <youssefahmedabdelkader25@gmail.com>
2024-08-13 22:02:11 +0300

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
+
+file{ 'ect/ssh/ssh_cofig':
+       ensure => present,
+
+content => "
+
+        #ssh client configuration
+        host*
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+}
